88f69596d896c3a6031b48518a5ae5044db0759b
thanhxuancoffeeandmore <thanhxuancoffeeandmore>
2020-07-03 10:16:17 +0700

    Update footer.php

--- application/views/frontend/modules/footer.php
+++ application/views/frontend/modules/footer.php
@@ -38,14 +38,7 @@
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-6">
-        <div class="col-xs-12 col-sm-6">
-            <h3 class="footer-title">HỖ TRỢ KHÁCH HÀNG</h3>
-            <ul class="list-unstyled linklists">
-                <li><a href="chinh-sach">Hướng dẫn thanh toán</a></li>          
-                <li><a href="chinh-sach">Hướng dẫn đặt hàng</a></li>
-                <li><a href="dieu-khoan">Điều khoản</a></li>                   
-            </ul>
-        </div>
+      
         <div class="col-xs-12 col-sm-6">
            <!-- <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=200&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="340" height="200" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe> -->
        </div>
